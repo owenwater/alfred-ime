5bc53d6aebe12d820f77e19404c9fa19df553875
Zhechen Min <owen200104@gmail.com>
2014-08-27 12:14:54 -0400

    move all code into src

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-from main import IMESelector
-
-if __name__=="__main__":
-    selector = IMESelector()
-    ime = selector.getIME(' '.join(sys.argv[1:]))
-    ime.execute()
